b87bae018a22353bab9e06d70faf2cd57434b510
Andrew Sharkey <sharkand@hotmail.com>
2021-03-27 09:56:22 +0000

    Added some styling to home page

--- controllers/controller.py
+++ controllers/controller.py
@@ -6,7 +6,7 @@ from models.game_round import GameRound
 
 @server.route('/')
 def index():
-    return ('Please select your path in the address bar, e.g. /rock/paper')
+    return render_template('index.html', title="home")
 
 @server.route('/<choice_1>/<choice_2>')
 def outcome(choice_1, choice_2):
@@ -20,11 +20,16 @@ def outcome(choice_1, choice_2):
     else:
         return (f'You both chose {current_round.player_1.choice}.  It is a draw!')
 
-
-
-
-
-
-
-
-
+@server.route('/play', methods=['post'])
+def start():
+    name_1 = request.form["player_1"]
+    name_2 = request.form["player_2"]
+    if name_1 == "":
+        name_1 = "Player1"
+    if name_2 == "":
+        name_2 = "Player2"
+    if name_1 == name_2:
+        return redirect('/')
+    player_1 = Player(name_1)
+    player_2 = Player(name_2)
+    return render_template('play.html', title = "play", player_1=player_1, player_2=player_2)
